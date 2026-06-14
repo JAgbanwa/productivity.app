57cd4bc23dce7eb587692bd8f7169a2eebf7e279
JAgbanwa <jamal.agbanwa@student.uantwerpen.be>
2026-06-15 00:11:43 +0200

    Update day1.cpp

--- day1.cpp
+++ day1.cpp
@@ -1,14 +1,1 @@
-#include <iostream>
-using namespace std;
 
-void main(){
-  cout << "Hello World! operator1 + operator2 = operator1 + operator2"
-}
-
-int operator1(){
- return 2;  
-} 
-
-int operator2 (){
-  return 114;
-}
